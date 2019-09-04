d627db3196353a562e7128e26b80a955f1c487dd
raechel <odom.raechel@gmail.com>
2019-09-03 18:14:45 -0600

    scraper code started

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-URL = 'https://www.amazon.com/PAPAISON-SPORTS-Adjustable-Outdoor-Rollerblades/dp/B07MKSPYKW/ref=sr_1_1_sspa?crid=3SEQ3APTZR60X&keywords=rollerblades+women&qid=1567553431&s=gateway&sprefix=rollerblades+%2Caps%2C211&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFJRUdBUkxFWkhQRzcmZW5jcnlwdGVkSWQ9QTAyODA4NjMzN1pYQ0xLVDcyQkE5JmVuY3J5cHRlZEFkSWQ9QTA3MTA3ODAxWkw0QTNJVlNUM0c3JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+URL = 'https://onewheel.com/products/pint#purchase'
 
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3876.0 Safari/537.36'}
 
-page = requests.get(url, headers=headers)
+page = requests.get(URL, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print()
+# title = soup.find()
+
+print(soup.prettify())
